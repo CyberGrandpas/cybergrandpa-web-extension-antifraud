c899a606a2a2fea5f6f772b54a868480d5e71381
Nessuno <35382039+nessuno71@users.noreply.github.com>
2025-02-15 10:03:22 +0100

    feat: Add loader state to overlay-loading-app with timeout for improved user experience during scanning.

--- src/components/apps/overlay-loading-app.svelte
+++ src/components/apps/overlay-loading-app.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import Modal from '@/components/modal.svelte';
 
+  let loader = $state(true);
   let { onClose } = $props();
-
   let t = i18n.t;
+
+  $effect(() => {
+    setTimeout(() => {
+      loader = false;
+    }, 1500);
+  });
 </script>
 
-<Modal autoShow loader logo {onClose}>
+<Modal logo autoShow {loader} {onClose}>
   <p>
     {t('global.scanning')}
   </p>
